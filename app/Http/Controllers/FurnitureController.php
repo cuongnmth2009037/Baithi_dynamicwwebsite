6d9be1a597a2a8a8834877e376c8b118e6ee0e63
t2009m1cuong <cuongnmth2009037@fpt.edu.vn>
2021-08-25 09:33:09 +0700

    nop bai thi dynamicWebsite

--- app/Http/Controllers/FurnitureController.php
+++ app/Http/Controllers/FurnitureController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\Furniture;
+use App\Models\Ajax;
 use Illuminate\Http\Request;
 
+
+class AjaxController extends Controller
+{
+    public function save(Request $request){
+        $ajax = new Ajax();
+        $ajax->fill($request->all());
+        $ajax->save();
+        return $ajax;
+    }
+    public function display(){
+        $ajax = Ajax::all();
+        return $ajax;
+    }
+}
 class FurnitureController extends Controller
 {
     /**
